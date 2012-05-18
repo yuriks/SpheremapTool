5841723a9a1a39ee4513226932a0ec837335c189
yuriks <yuriks@yuriks.net>
2012-05-18 15:26:02 -0300

    Added diagnostic when file opening fails.

--- src/main.cpp
+++ src/main.cpp
@@ -38,7 +38,10 @@ struct Image {
 
 	Image(const std::string& filename) :
 		data(stbi_load(filename.c_str(), &width, &height, nullptr, 4), stbi_image_free)
-	{}
+	{
+		if (data == nullptr)
+			std::cerr << "Failed to open " << filename << ".\n";
+	}
 
 	Image& operator= (Image&& o) {
 		width = o.width;
